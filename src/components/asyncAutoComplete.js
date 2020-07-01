b7ea07b4f5eae8433a8d62b5c5d7cdd29f4ce2c0
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-07-01 18:54:52 +0600

    Fix Autocomplete fetch api no result data show event and Add button for new add

--- src/components/asyncAutoComplete.js
+++ src/components/asyncAutoComplete.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Button } from "@material-ui/core";
 
 function sleep(delay = 0) {
   return new Promise(resolve => {
@@ -12,8 +13,25 @@ function sleep(delay = 0) {
 export default function AsyncAutoComplete(props) {
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState([]);
+  
+
+  const [inputData, setInputData] = React.useState("");
+  const [showButton, setAdd] = React.useState(false);
 
   const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    console.log("hasNoData");
+   }, [showButton]);
+
+  useEffect ( () => {
+    
+    if(options.some( e => e[props.selectName] === inputData) && options.length !== 0){
+      setAdd(false);
+    }else{
+      setAdd(true);
+    }
+  },[inputData]);
 
   React.useEffect(() => {
     let active = true;
@@ -26,9 +44,12 @@ export default function AsyncAutoComplete(props) {
       const response = await fetch(
         props.fetchUrl
       );
-      await sleep(1e3); // For demo purposes.
+      await sleep(4); // For demo purposes.
       const datas = await response.json();
-
+      if (datas.length == 0){
+        setAdd(true);
+        setOptions([{[props.selectName] : "No Data to Show"}]);
+      }
       if (active) {
         setOptions(datas);
       }
@@ -41,11 +62,13 @@ export default function AsyncAutoComplete(props) {
 
   React.useEffect(() => {
     if (!open) {
+      setAdd(false);
       setOptions([]);
     }
   }, [open]);
 
   const handleAutoComplete = (name,value) => {
+    console.log(value);
     props.onDataChange(name,value);
   }
   return (
@@ -63,27 +86,35 @@ export default function AsyncAutoComplete(props) {
       onChange={(event,value) => handleAutoComplete(props.name,value)} // prints the selected value
       getOptionSelected={(option, value) => option[props.selectName] === option[props.selectName]}
       getOptionLabel={option => option[props.selectName]}
+      getOptionDisabled={(option) => option[props.selectName] === "No Data to Show"}
       options={options}
       loading={loading}
       renderInput={params => (
-        <TextField
-          {...params}
-          label={props.label}
-          name={props.name}
-          variant="outlined"
-          required={props.required}
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {loading ? (
-                  <CircularProgress color="inherit" size={20} />
-                ) : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            )
-          }}
-        />
+        <div>
+          <TextField
+            {...params}
+            label={props.label}
+            name={props.name}
+            variant="outlined"
+            required={props.required}
+            onChange={e => setInputData(e.target.value)}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : () => alert("not found")}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              )
+            }}
+          />
+         { showButton && <Button size="small" style={{marginTop:"-28%",marginLeft:"105%"}} variant="outlined" color="primary">
+            Add
+          </Button>
+          }
+        </div>
       )}
     />
   );
