49a5f1d2c4ec0b56a2c08126ea34176446fbb54f
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-09-04 17:55:23 +0600

    complete purchase report by date route

--- inventory_rest_api/Controllers/ReportController.cs
+++ inventory_rest_api/Controllers/ReportController.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using inventory_rest_api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace inventory_rest_api.Controllers
 {
@@ -31,14 +34,14 @@ namespace inventory_rest_api.Controllers
                                     Date = AppUtils.DateTime(purchase.PurchaseDate).ToShortDateString(),
                                 };
             var purRateReport = purchaseRateQuery.AsEnumerable()
-                        .GroupBy( 
+                        .GroupBy(
                             p => p.Date,
-                            (key,g) => new { 
-                                Date = key , 
+                            (key,g) => new {
+                                Date = key ,
                                 Count = g.Count(),
                                 Data = g.ToList(),
                             }
-                        ).ToList();      
+                        ).ToList();
 
             var report = new {
                 TotalProductPurchase  = _context.Purchases.Select( p => p.ProductId).Distinct().Count(),
@@ -51,6 +54,36 @@ namespace inventory_rest_api.Controllers
             return report;
         }
 
+
+        [HttpGet("purchase-report-all")]
+        public  ActionResult<Object> GetPurchaseForRepo(string date){
+            var purchaseRateQuery = from purchase in _context.Purchases
+                                    select new {
+                                        purchase.PurchaseId,
+                                        purchase.ProductId,
+                                        purchase.ProductQuantity,
+                                        purchase.PurchasePrice,
+                                        purchase.PurchasePaidStatus,
+                                        AppUtils.DateTime(purchase.PurchaseDate).Day,
+                                        AppUtils.DateTime(purchase.PurchaseDate).Month,
+                                        AppUtils.DateTime(purchase.PurchaseDate).Year,
+                                        Date = AppUtils.DateTime(purchase.PurchaseDate).ToShortDateString(),
+                                    };
+            var purRateReport = purchaseRateQuery.AsEnumerable()
+                        .GroupBy(
+                            p => p.Date,
+                            (key,g) => new {
+                                Date = key ,
+                                Count = g.Count(),
+                                Data = g.ToList(),
+                            }
+                        ).ToList();
+            var report = new {
+                TotalPurchaseProductDue =  _context.PurchaseDueProducts.Include(pd => pd.Purchase),
+                PurchaseRate = purRateReport,
+            };
+            return report;
+        }
         [HttpGet("purchase-report/{date}")]
         public  ActionResult<Object> GetPurchaseReport(string date){
 
@@ -67,21 +100,21 @@ namespace inventory_rest_api.Controllers
                                         Date = AppUtils.DateTime(purchase.PurchaseDate).ToShortDateString(),
                                     };
             var purQuery = purchaseRateQuery.AsEnumerable().Where(p => p.Date == AppUtils.DateTime(date).ToShortDateString() )
-                        .ToList();  
+                            .ToList();
 
             var purRateReport = purchaseRateQuery.AsEnumerable()
-                        .GroupBy( 
+                        .GroupBy(
                             p => p.Date,
-                            (key,g) => new { 
-                                Date = key , 
+                            (key,g) => new {
+                                Date = key ,
                                 Count = g.Count(),
                                 Data = g.ToList(),
                             }
-                        ).ToList();  
+                        ).ToList();
             var report = new {
                 TotalProductPurchase  = purQuery.Select(p => p.ProductId).Distinct().Count(),
                 TotalPurchasePrice = purQuery.Sum( p => p.PurchasePrice),
-                TotalPurchaseProductDue = purQuery.Select( pd => pd.ProductId).Distinct().Count(),
+                TotalPurchaseProductDue = _context.PurchaseDueProducts.Where(pd => pd.Purchase.PurchaseDate == AppUtils.DateTime(date).ToShortDateString()).Select( pd => pd.Purchase.ProductId).Distinct().Count(),
                 TotalPurchasePaymentDue = purQuery.Where( p => p.PurchasePaidStatus == false).Count(),
                 PurchaseRate = purRateReport,
             };
