3de0aa5e6a9ee36702fbc515a1c54b4bd6167c5a
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-09-27 13:21:08 +0600

    Add Damage Get Date Range route

--- inventory_rest_api/Controllers/DamagesController.cs
+++ inventory_rest_api/Controllers/DamagesController.cs
@@ -115,6 +115,137 @@ namespace inventory_rest_api.Controllers
             return damage;
         }
 
+        [HttpGet("by-date-range/{date1}-{date2}")]
+        public ActionResult<IEnumerable> GetDamagesByDateRange(string date1,string date2){
+            
+            double d1 = double.Parse(date1);
+            double d2 = double.Parse(date2);
+
+            return  _context.Damages
+                                .Include(d => d.Product)
+                                .Include(d => d.Customer)
+                                .Include(d => d.Supplier)
+                                .Include(d => d.Employee)
+                                .Select( d => new {
+                                    d.DamageId,
+
+                                    d.ProductId,
+                                    d.Product.ProductName,
+
+                                    d.CustomerId,
+                                    d.Customer.CustomerName,
+
+                                    d.EmployeeId,
+                                    d.Employee.EmployeeName,
+
+                                    d.SupplierId,
+                                    d.Supplier.SupplierName,
+                                    
+                                    d.DamageRetDate,
+                                    DamageRetDateLong = double.Parse(d.DamageRetDate),
+
+                                    d.ProductQuantity,
+                                    d.DamageProductAmount,
+                                    d.DamageSentToCompanyStatus,
+                                    d.DamageSentToCompanyDate,
+                                    d.DamageRetFromCompanyDate,
+                                    d.DamageRetFromComAmount,
+                                    d.DamageRetComProQuantity,
+                                    d.DamageRetComProQuantityDueStatus,
+                                    d.DamgeReturnCompanyDueAmount,
+                                    d.DamgeReturnCompanyDuePaymentStatus,
+                                    d.DamgeReturnCompanyDueDate
+                                })
+                                .AsEnumerable()
+                                .Where( d => d.DamageRetDateLong >= d1 && d.DamageRetDateLong < d2 )
+                                .ToList();
+        }
+
+        [HttpGet("by-date/{date}")]
+        public ActionResult<IEnumerable> GetDamagesByDate(string date){
+    
+            return  _context.Damages
+                            .Include(d => d.Product)
+                            .Include(d => d.Customer)
+                            .Include(d => d.Supplier)
+                            .Include(d => d.Employee)
+                            .Select( d => new {
+                                d.DamageId,
+                                d.ProductId,
+                                d.Product.ProductName,
+                                d.CustomerId,
+                                d.Customer.CustomerName,
+                                d.EmployeeId,
+                                d.Employee.EmployeeName,
+                                d.SupplierId,
+                                d.Supplier.SupplierName,
+                                
+                                d.DamageRetDate,
+                                d.ProductQuantity,
+                                d.DamageProductAmount,
+                                d.DamageSentToCompanyStatus,
+                                d.DamageSentToCompanyDate,
+                                d.DamageRetFromCompanyDate,
+                                d.DamageRetFromComAmount,
+                                d.DamageRetComProQuantity,
+                                d.DamageRetComProQuantityDueStatus,
+                                d.DamgeReturnCompanyDueAmount,
+                                d.DamgeReturnCompanyDuePaymentStatus,
+                                d.DamgeReturnCompanyDueDate
+                            })
+                            .AsEnumerable()
+                            .Where( d => AppUtils.DateTime(d.DamageRetDate).ToShortDateString() == AppUtils.DateTime(date).ToShortDateString())
+                            .ToList();
+        }
+
+        [HttpGet("by-days/{days}")]
+        public ActionResult<IEnumerable> GetDamagesByDays(int days){
+            
+            List<String> valDate = new List<string>();
+            for (int i = 0; i < days; i++)
+            {
+                long milliseconds = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+                string dateTime =  AppUtils.DateTime(milliseconds-86400000*i ).ToShortDateString();
+                valDate.Add(dateTime);
+            }
+            return  _context.Damages
+                                .Include(d => d.Product)
+                                .Include(d => d.Customer)
+                                .Include(d => d.Supplier)
+                                .Include(d => d.Employee)
+                                .Select( d => new {
+                                    d.DamageId,
+
+                                    d.ProductId,
+                                    d.Product.ProductName,
+
+                                    d.CustomerId,
+                                    d.Customer.CustomerName,
+
+                                    d.EmployeeId,
+                                    d.Employee.EmployeeName,
+
+                                    d.SupplierId,
+                                    d.Supplier.SupplierName,
+                                    
+                                    d.DamageRetDate,
+                                    d.ProductQuantity,
+                                    d.DamageProductAmount,
+                                    d.DamageSentToCompanyStatus,
+                                    d.DamageSentToCompanyDate,
+                                    d.DamageRetFromCompanyDate,
+                                    d.DamageRetFromComAmount,
+                                    d.DamageRetComProQuantity,
+                                    d.DamageRetComProQuantityDueStatus,
+                                    d.DamgeReturnCompanyDueAmount,
+                                    d.DamgeReturnCompanyDuePaymentStatus,
+                                    d.DamgeReturnCompanyDueDate
+                                })
+                                .AsEnumerable()
+                                .Where( d => valDate.Contains( AppUtils.DateTime(d.DamageRetDate).ToShortDateString()) )
+                                .ToList();
+        }
         // PUT: api/Damages/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
