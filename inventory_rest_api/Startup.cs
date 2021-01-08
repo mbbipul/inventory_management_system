5b7930ccddc63798ba780e2d40a5b067751c3532
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2021-01-08 20:55:19 +0600

    Update database deployment and change database connection string , Add migratioon to code

--- inventory_rest_api/Startup.cs
+++ inventory_rest_api/Startup.cs
@@ -103,10 +103,10 @@ namespace inventory_rest_api
                 endpoints.MapControllers();
             });
 
-            // using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            // {
-            //     scope.ServiceProvider.GetService<InventoryDbContext>().Database.Migrate();
-            // }
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetService<InventoryDbContext>().Database.Migrate();
+            }
         //   SeedData.EnsurePopulated(app);
         }
     }
